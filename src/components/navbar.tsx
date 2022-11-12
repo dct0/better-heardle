c75149f9ea3b6ec9fdef84deb18dd3cd36ed40e7
Dylan To <dylan.to@hotmail.com>
2022-11-12 21:59:09 +0800

    playlists fetching

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,25 @@ import { AppConfig } from '@/utils/AppConfig';
 
 import LoginButton from './loginbutton';
 
+const getPlaylists = () => {
+  fetch('/api/playlists', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${window.localStorage.getItem('access_token')}`,
+    },
+  }).then((res) =>
+    res
+      .json()
+      .then((r) => {
+        console.log(r);
+        window.localStorage.setItem('playlists', JSON.stringify(r.items));
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  );
+};
+
 const Navbar = () => {
   return (
     <nav className="border-b-2 border-black p-2">
@@ -15,7 +34,12 @@ const Navbar = () => {
 
         <div className="font-bold">{AppConfig.title}</div>
         <span className="flex">
-          <FaChartBar className="m-2" />
+          <FaChartBar
+            className="m-2"
+            onClick={() => {
+              getPlaylists();
+            }}
+          />
           <LoginButton />
         </span>
       </div>
--- src/pages/api/playlists.ts
+++ src/pages/api/playlists.ts
@@ -1,33 +1,40 @@
 import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // receive a token
   // here, our secret is stored server-side in an environment variable
   // token is to be stored in a cookie maybe?
   // send back
   if (req.method !== 'GET') {
-    res.status(400);
+    res.status(405).send({ message: 'Only GET requests allowed' });
+  }
+
+  if (req.body.token) {
+    res.status(400).send('Missing access token');
   }
 
   const axiosConfig = {
     headers: {
-      Authorization: `Bearer ${req.body.token}`,
-      'Content-Type': 'application/json',
+      Authorization: req.headers.authorization,
     },
   };
 
-  axios
-    .get('https://api.spotify.com/v1/me/playlists', axiosConfig)
-    .then((r) => {
-      if (r.status !== 200) {
-        console.error('Could not get playlists');
-        // also error
-      }
-
-      console.log(r.data);
-    });
-  res.status(200).json({ name: 'Test' });
+  try {
+    const r = await axios.get(
+      'https://api.spotify.com/v1/me/playlists',
+      axiosConfig
+    );
+    res.status(r.status).json(r.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.status && error.response)
+        res.status(error.status).json(error.response.data);
+    } else {
+      res.status(500).json(error);
+      console.error(error);
+    }
+  }
 };
 
 export default handler;
